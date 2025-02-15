adc7d1007f41569d7a034f65dc735a56906e2c31
Colonel-Aziret <Aziret5265@gmail.com>
2025-02-15 19:53:26 +0600

    initial commit

---
+++ src/main/java/com/example/rentcarkg/RentCarKgApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentcarkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCarKgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentCarKgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentcarkg/RentCarKgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentcarkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarKgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
