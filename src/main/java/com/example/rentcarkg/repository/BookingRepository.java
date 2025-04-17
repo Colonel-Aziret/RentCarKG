6d1e8142b3a348a057cbe2f18301e8e533183839
Азирет Раманкулов <Aziret5265@gmail.com>
2025-04-17 17:23:38 +0600

    feat: add method to fetch bookings by car owner and status

--- src/main/java/com/example/rentcarkg/repository/BookingRepository.java
+++ src/main/java/com/example/rentcarkg/repository/BookingRepository.java
@@ -4,6 +4,8 @@ import com.example.rentcarkg.enums.BookingStatus;
 import com.example.rentcarkg.model.Booking;
 import com.example.rentcarkg.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,6 +17,15 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByCarIdAndStatus(Long carId, BookingStatus status);
 
     List<Booking> findByCarIdAndStatusIn(Long carId, List<BookingStatus> statuses);
+
+    @Query("""
+                SELECT b FROM Booking b
+                JOIN FETCH b.car c
+                JOIN FETCH c.owner o
+                WHERE o.email = :ownerEmail AND b.status = 'EMAIL_CONFIRMED'
+            """)
+    List<Booking> findAllForOwnerEmailConfirmed(@Param("ownerEmail") String ownerEmail);
+
 
     List<Booking> findByUserAndStatus(User user, BookingStatus bookingStatus);
 }
