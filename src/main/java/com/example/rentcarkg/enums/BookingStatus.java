b62d0180306869054d4e9786c0f3ee2f70f3da73
Азирет Раманкулов <Aziret5265@gmail.com>
2025-04-17 17:23:17 +0600

    chore: add new status EMAIL_CONFIRMED and CANCELLED to support email confirmation and cancellation flow

--- src/main/java/com/example/rentcarkg/enums/BookingStatus.java
+++ src/main/java/com/example/rentcarkg/enums/BookingStatus.java
@@ -1,6 +1,12 @@
 package com.example.rentcarkg.enums;
 
 public enum BookingStatus {
-    PENDING, CONFIRMED, REJECTED, CANCELLED
+    PENDING,            // заявка создана
+    EMAIL_CONFIRMED,    // клиент подтвердил по почте
+    CONFIRMED,          // владелец подтвердил
+    REJECTED,           // отклонено владельцем
+    CANCELLED,           // отменено клиентом
+    EXPIRED             // автоотмена
 }
+
 
