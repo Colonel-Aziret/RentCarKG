719529916e7a5b04ae8f9bd02f92f63fe45991b8
Colonel-Aziret <Aziret5265@gmail.com>
2025-05-29 00:52:18 +0600

    fix: "add some variables"

--- src/main/java/com/example/rentcarkg/dto/request/CarRequest.java
+++ src/main/java/com/example/rentcarkg/dto/request/CarRequest.java
@@ -1,6 +1,7 @@
 package com.example.rentcarkg.dto.request;
 
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -11,10 +12,14 @@ public record CarRequest(
         @NotBlank(message = "Model is required") String model,
         @NotNull(message = "Year is required") int year,
         @NotBlank(message = "Color is required") String color,
+        @NotNull(message = "Capacity is required") @Min(value = 1, message = "Capacity must be at least 1") int capacity,
+        @NotBlank(message = "Fuel type is required") String fuelType,
+        @NotBlank(message = "Transmission type is required") String transmission,
         @NotBlank(message = "Description is required") String description,
         @NotNull(message = "Price per day is required")
         @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
         BigDecimal pricePerDay
 ) {
 }
+
 
